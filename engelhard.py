3192f3b29b0bae874ba97722d293573bf9b3bca1
Rob Pongsajapan <rap42@georgetown.edu>
2015-10-02 14:58:58 -0400

    version for site

--- engelhard.py
+++ engelhard.py
@@ -10,7 +10,7 @@ def prerender_jinja(text):
   prerendered_body = render_template_string(Markup(text))
   return pygmented_markdown(prerendered_body)
 
-APP_PREFIX='/engelhard/'
+APP_PREFIX='/'
 app.config.update(dict(
   FREEZER_DESTINATION = 'build',
   FREEZER_DESTINATION_IGNORE = ['.GIT*', 'CNAME', '.gitignore', 'readme.md'],
@@ -19,16 +19,29 @@ app.config.update(dict(
   FLATPAGES_HTML_RENDERER = prerender_jinja,
 ))
 
+profiles = []
+for p in pages:
+  if ('profiles' in p.path) and p.body:
+    profiles.append(p)
+profiles = sorted(profiles, key=lambda k: (k.meta['faculty']))
+
 # routes
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
+@app.route('/profiles/')
+def profile_list():
+  return render_template('profile_list.html', profiles=profiles)
+
 @app.route('/<path:path>/')
 def page(path):
   page = pages.get_or_404(path)
-  template = page.meta.get('template', 'page.html')
+  if 'profiles' in page.path:
+    template = page.meta.get('template', 'profile_detail.html')
+  else:
+    template = page.meta.get('template', 'page.html')
   return render_template(template, page=page)
 
 if __name__ == '__main__':
--- freeze.py
+++ freeze.py
@@ -1,7 +1,14 @@
 from flask_frozen import Freezer
+from flask_flatpages import FlatPages
 from engelhard import app
 
+pages = FlatPages(app)
+
 freezer = Freezer(app)
+
+@freezer.register_generator
+def profile_details():
+  yield '/profiles/elmendorf/'
 
 if __name__ == '__main__':
     freezer.freeze()
